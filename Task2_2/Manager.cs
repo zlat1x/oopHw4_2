c323bc0128e240876533c1ddaad2ff0ca19fe970
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-11 00:37:24 +0200

    feat(manager): random schedule 1..10 calls, relax, 1..5 calls

---
+++ Task2_2/Manager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2_2
+{
+    public class Manager : Worker
+    {
+        private static readonly Random Rng = new Random();
+
+        public Manager(string name) : base(name) { Position = "Менеджер"; }
+
+        // Call x[1..10] - Relax - Call x[1..5]
+        public override void FillWorkDay()
+        {
+            ResetDay();
+
+            int c1 = Rng.Next(1, 11);
+            for (int i = 0; i < c1; i++) Call();
+
+            Relax();
+
+            int c2 = Rng.Next(1, 6);
+            for (int i = 0; i < c2; i++) Call();
+        }
+    }
+}

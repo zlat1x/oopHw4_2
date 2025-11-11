acbf48753905b273be4e4793dca7e5f5bff1db22
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-11 09:58:16 +0200

    fix(task5): add using for ObservableCollection in MainViewModel

--- lab5_maui/Lab5Maui/ViewModels/MainViewModel.cs
+++ lab5_maui/Lab5Maui/ViewModels/MainViewModel.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;  
 using Lab5Maui.Models;
 
 namespace Lab5Maui.ViewModels
@@ -12,43 +10,30 @@ namespace Lab5Maui.ViewModels
 
         public MainViewModel()
         {
-            Items.Add(new FoodProduct
-            {
-                Name = "Milk 2.5%",
-                Price = 35,
-                CountryOfOrigin = "UA",
+            Items.Add(new FoodProduct {
+                Name = "Milk 2.5%", Price = 35, CountryOfOrigin = "UA",
                 PackingDate = DateTime.Today.AddDays(-1),
                 Description = "Dairy",
                 ExpirationDate = DateTime.Today.AddDays(5),
-                Quantity = 1,
-                Unit = "L"
+                Quantity = 1, Unit = "L"
             });
 
-            Items.Add(new Book
-            {
-                Name = "Clean Code",
-                Price = 650,
-                CountryOfOrigin = "UA",
+            Items.Add(new Book {
+                Name = "Clean Code", Price = 650, CountryOfOrigin = "UA",
                 PackingDate = DateTime.Today,
                 Description = "Programming",
-                Pages = 464,
-                Publisher = "Prentice Hall",
-                Authors = { "Robert C. Martin" }
+                Pages = 464, Publisher = "Prentice Hall", Authors = { "Robert C. Martin" }
             });
         }
 
         public void AddSample()
         {
-            Items.Add(new FoodProduct
-            {
-                Name = "Bread",
-                Price = 25,
-                CountryOfOrigin = "UA",
+            Items.Add(new FoodProduct {
+                Name = "Bread", Price = 25, CountryOfOrigin = "UA",
                 PackingDate = DateTime.Today,
                 Description = "Bakery",
                 ExpirationDate = DateTime.Today.AddDays(3),
-                Quantity = 1,
-                Unit = "pc"
+                Quantity = 1, Unit = "pc"
             });
         }
 

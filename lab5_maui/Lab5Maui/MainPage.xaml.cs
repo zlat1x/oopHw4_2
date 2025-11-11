22da4033aa5b97d0c5f506f6e1b20089ba9d1d56
Zlata Dovbyk <dovbykzlata@gmail.com>
2025-11-11 09:48:04 +0200

    feat(lab5): main page with grid-like list, Add/Delete buttons, and ViewModel

--- lab5_maui/Lab5Maui/MainPage.xaml.cs
+++ lab5_maui/Lab5Maui/MainPage.xaml.cs
@@ -1,23 +1,17 @@
-﻿namespace Lab5Maui;
+﻿using Lab5Maui.ViewModels;
+
+namespace Lab5Maui;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private readonly MainViewModel _vm = new();
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = _vm;
+    }
 
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void Add_Clicked(object sender, EventArgs e) => _vm.AddSample();
+    private void Remove_Clicked(object sender, EventArgs e) => _vm.RemoveLast();
 }
---
+++ lab5_maui/Lab5Maui/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab5Maui.Models;
+
+namespace Lab5Maui.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Product> Items { get; } = new();
+
+        public MainViewModel()
+        {
+            Items.Add(new FoodProduct
+            {
+                Name = "Milk 2.5%",
+                Price = 35,
+                CountryOfOrigin = "UA",
+                PackingDate = DateTime.Today.AddDays(-1),
+                Description = "Dairy",
+                ExpirationDate = DateTime.Today.AddDays(5),
+                Quantity = 1,
+                Unit = "L"
+            });
+
+            Items.Add(new Book
+            {
+                Name = "Clean Code",
+                Price = 650,
+                CountryOfOrigin = "UA",
+                PackingDate = DateTime.Today,
+                Description = "Programming",
+                Pages = 464,
+                Publisher = "Prentice Hall",
+                Authors = { "Robert C. Martin" }
+            });
+        }
+
+        public void AddSample()
+        {
+            Items.Add(new FoodProduct
+            {
+                Name = "Bread",
+                Price = 25,
+                CountryOfOrigin = "UA",
+                PackingDate = DateTime.Today,
+                Description = "Bakery",
+                ExpirationDate = DateTime.Today.AddDays(3),
+                Quantity = 1,
+                Unit = "pc"
+            });
+        }
+
+        public void RemoveLast()
+        {
+            if (Items.Count > 0) Items.RemoveAt(Items.Count - 1);
+        }
+    }
+}
